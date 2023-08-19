7dcaa8902c77b35ab8437b7a863e0c0e1b6b6c67
Laudelino M C Neto <laudenpower@gmail.com>
2023-08-18 22:10:16 -0300

    Implementado pré-requisitos para desenvolvimento da regra de negócio

--- src/main/java/br/com/senai/usuariosmarketplace/InitApp.java
+++ src/main/java/br/com/senai/usuariosmarketplace/InitApp.java
@@ -1,12 +1,15 @@
 package br.com.senai.usuariosmarketplace;
 
-import br.com.senai.usuariosmarketplace.core.dao.ManagerDb;
+import br.com.senai.usuariosmarketplace.core.service.UsuarioService;
 
 public class InitApp {
 
 	public static void main(String[] args) {
-		ManagerDb.getInstance().getConexao();
-		System.out.println("Conectou");
+		UsuarioService service = new UsuarioService();
+		System.out.println(service.removerAcentoDo("Jacó da Silva"));
+		System.out.println(service.fracionar("José da Silva Alburquerque dos Anjos e Bragança"));
+		System.out.println(service.gerarLoginPor("José da Silva dos Anjos"));
+		System.out.println(service.gerarHashDa("jose123456"));
 	}
 
 }
